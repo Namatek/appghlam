42b4e1e468d4d5abb60e0bbfd7626825ed12f7a9
Haj Rezvan <rezvanianragahi2@gmail.com>
2024-11-18 00:27:40 +0330

    🔧: Remove additional opptions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App" style={{ textAlign: 'center', margin: '20px' }}>
       <iframe
         src="https://27malek.ir/app/application/login-672a052f295ebf541b14d756?embed=true" // Replace with the URL you want to display
-        title="Example Iframe"
         width="100%"
         height="100%"
         style={{ border: 'none' }}
