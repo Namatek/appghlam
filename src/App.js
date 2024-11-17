05e0461d3f3e43176ef8d6dd97167c091709ba95
Haj Rezvan <58388933+hajrezvan@users.noreply.github.com>
2024-11-18 00:12:00 +0330

    🔧: First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ textAlign: 'center', margin: '20px' }}>
+      <iframe
+        src="https://27malek.ir/app/application/login-672a052f295ebf541b14d756?embed=true" // Replace with the URL you want to display
+        title="Example Iframe"
+        width="100%"
+        height="600px"
+        style={{ border: 'none' }}
+      />
+    </div>
+  );
+}
+
+export default App;
